4f9373dc644c9f594b409eb318f1ecd246876fd5
Luís Miguel Cabral <luis@stibodx.com>
2023-03-02 11:17:06 +0100

    fixup! demonstrating an exception occurs when using wdio browser overrideCommand()

--- tests/specs/local_test.js
+++ tests/specs/local_test.js
@@ -1,5 +1,13 @@
-describe('BrowserStack Local Testing', () => {
+describe('BrowserStack Local Testing', async () => {
+
+
   it('can check tunnel working', async () => {
+
+    await browser.overwriteCommand('url', async function (originalFunction, path) {
+      console.info(`overwriteCommand browser.url(${path})`)
+      return originalFunction(path);
+    });
+
     await browser.url('http://bs-local.com:45454');
     await browser.waitUntil(
       async () => (await browser.getTitle()).match(/BrowserStack Local/i),
@@ -7,4 +15,17 @@ describe('BrowserStack Local Testing', () => {
       "Failed to connect local tunnel"
     );
   });
+});
+
+/**
+ * An unhandledRejection is caught here.
+ * In our system, unhandledExceptions cause the process to terminate and exit and
+ * we  would prefer not to have to catch unhandled exceptions like this as it
+ * has the potential to pollute the error output.
+ *
+ * IMHO the exception should be fixed and could be done so by adding an if condition
+ * to https://github.com/webdriverio/webdriverio/blob/main/packages/wdio-browserstack-service/src/insights-handler.ts#L229-L247
+ */
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', {error});
 });
